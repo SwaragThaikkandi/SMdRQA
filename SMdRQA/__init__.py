3360b891cf62a3e9847be6d8ddc1af7626c51250
SwaragThaikkandi <142320710+SwaragThaikkandi@users.noreply.github.com>
2024-03-22 14:48:15 +0530

    Add files via upload

---
+++ SMdRQA/__init__.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+__docformat__ = "restructuredtext"
+
+__version__ = "0.0.1"
+
+import numpy as np
+from scipy.integrate import solve_ivp
+import operator
+import contextlib
+import functools
+import operator
+import warnings
+from numpy.core import overrides
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+import pandas as pd
+from os import listdir
+from os.path import isfile, join
+from collections import defaultdict
+import csv
+from tqdm import tqdm
+import os
+import numpy as np
+import operator
+import contextlib
+import functools
+import operator
+import warnings
+from numpy.core import overrides
+import matplotlib.pyplot as plt
+import pandas as pd
+from os import listdir
+from os.path import isfile, join
+from collections import defaultdict
+import csv
+from tqdm import tqdm
+import pickle
+import random
+from scipy.stats import skew
+from p_tqdm import p_map
+from functools import partial
+from scipy.interpolate import pchip_interpolate
+import memory_profiler
+import ast
+
+from kuramoto import Kuramoto, plot_phase_coherence, plot_activity # see https://github.com/fabridamicelli/kuramoto
+# For installing the kuramoto package run: pip install kuramoto
+
+
