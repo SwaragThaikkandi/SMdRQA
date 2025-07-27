5d0ebbde31a8c5ed3134f2c4b099a54438ea8541
Swarag Thaikkandi <142320710+SwaragThaikkandi@users.noreply.github.com>
2025-07-27 10:38:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="SMdRQA",
-    version="2025.7.23",
+    version="2025.7.27",
     author="Swarag Thaikkandi, K.M. Sharika, Miss Nivedita",
     author_email="tsk4at@gmail.com",
     description="A Python implementation of MdRQA",
